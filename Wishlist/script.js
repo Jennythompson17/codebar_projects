ca88b49d88dbf74984c63d12c0d52823e931f836
JennyThompson17 <jennythompson17@gmail.com>
2014-02-01 12:00:38 +0000

    Trying to add function to add wishes

--- Wishlist/script.js
+++ Wishlist/script.js
@@ -1,1 +1,28 @@
 /* Exercise 1: Wish list */
+
+//revising Lesson 2
+
+function listDomElements() {
+  var children = document.body.childNodes;
+  for(var i=0; i <  children.length; i++) {
+    console.log(children[i]);
+  }
+}
+
+
+function addWish() {
+  var wish = document.createElement("wish");
+  var content = document.createTextNode($("input[type=text]"););
+  wish.appendChild(content);
+  document.body.appendChild(population);
+}
+
+$(document).ready(function() {
+  // here go all the interactions
+
+  /* click, mouseover, change etc */
+});
+
+
+
+//$('#items').append("<li>Thing One</li>")
