90ce4ddce5c62f5ee4876e9e393502b4a2573268
JennyThompson17 <jennythompson17@gmail.com>
2014-02-01 19:25:12 +0000

    cleaned up workings

--- Wishlist/script.js
+++ Wishlist/script.js
@@ -1,19 +1,5 @@
 /* Exercise 1: Wish list */
 
-//revising Lesson 2
-
-//function listDomElements() {
-  //var children = document.body.childNodes;
-  //for(var i=0; i <  children.length; i++) {
-    //console.log(children[i]);
-  //}
-//}
-
-
-
-//function add() {
-//	$('ol#items').append($('input').val()) 
-//}
 
 //var item = $('#item').val();
 function addToList(item) {
@@ -27,10 +13,3 @@ function addToList(item) {
 
 //JQuery method
 //$(document).ready(addToList())
-
-
-//$('input').val("wish 1");
-
-//$('#items').append("<div>Wish 1</div>")
-
-//$('#items').append("<li>Thing One</li>")
