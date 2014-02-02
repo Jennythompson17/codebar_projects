30206a7f99b44494840477f41162101dc5de0152
JennyThompson17 <jennythompson17@gmail.com>
2014-02-02 10:39:41 +0000

    trying to change pending label to complete

--- Wishlist/script.js
+++ Wishlist/script.js
@@ -5,11 +5,23 @@
 function addToList(item) {
 	$(document).on('click','#add-to-list' , function() {
 		$('ol#items').append("<li>"+$('#item').val()+"</li>").append("<span class='label pending'>Pending</span>")
+		$(this).on('click', '.label pending', function() {
+			$(this).addClass('complete')
+			$(this).append("<span class='label success'>Done!</span>")
+			}
 		$('#item').val("") 
 		$('#item').focus()
+		});
+	}
+
+//function completeItem(item) {
+	$(document).on('click', '.label pending', function() {
+		$(this).addClass('.complete');
+		$(this).append("<span class='label success'>Done!</span>");
 		});
 	}
 
 
 //JQuery method - wrapped to wait for page load
 $(document).ready(addToList())
+//$(document).ready(completeItem())
