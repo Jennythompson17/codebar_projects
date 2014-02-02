981b42001d8ee63b8e2d140f475fed834ed94c5b
JennyThompson17 <jennythompson17@gmail.com>
2014-02-02 10:40:49 +0000

    reverting - didn't work

--- Wishlist/script.js
+++ Wishlist/script.js
@@ -5,21 +5,17 @@
 function addToList(item) {
 	$(document).on('click','#add-to-list' , function() {
 		$('ol#items').append("<li>"+$('#item').val()+"</li>").append("<span class='label pending'>Pending</span>")
-		$(this).on('click', '.label pending', function() {
-			$(this).addClass('complete')
-			$(this).append("<span class='label success'>Done!</span>")
-			}
 		$('#item').val("") 
 		$('#item').focus()
 		});
 	}
 
 //function completeItem(item) {
-	$(document).on('click', '.label pending', function() {
-		$(this).addClass('.complete');
-		$(this).append("<span class='label success'>Done!</span>");
-		});
-	}
+	//$(document).on('click', '.label pending', function() {
+		//$(this).addClass('.complete');
+		//$(this).append("<span class='label success'>Done!</span>");
+		//});
+	//}
 
 
 //JQuery method - wrapped to wait for page load
