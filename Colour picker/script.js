8d371d34ae86f127db3c130180bdfa28121a78d1
JennyThompson17 <jennythompson17@gmail.com>
2014-02-12 20:54:01 +0000

    Set the background colour to green

--- Colour picker/script.js
+++ Colour picker/script.js
@@ -1,1 +1,15 @@
 /* Exercise 2: Color picker */
+
+
+// set background color
+function setPreviewColor(color) {
+	$(document).on('click change','.preview', function() {
+		$(this).css('background-color','green' )
+	}); 
+}
+
+//Input text and keypress or keydown or keyup
+//Grab the preview div
+//Change the color class
+
+$(document).ready(setPreviewColor())
