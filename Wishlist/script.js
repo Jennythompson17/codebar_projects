c518980efd37c40486a6ec49150c3d6792e387f3
JennyThompson17 <jennythompson17@gmail.com>
2014-02-12 20:14:46 +0000

    finished wishlist

--- Wishlist/script.js
+++ Wishlist/script.js
@@ -1,24 +1,50 @@
 /* Exercise 1: Wish list */
 
 
-// Question for Codebar - how do I get the label on the same line?
+
 function addToList(item) {
 	$(document).on('click','#add-to-list' , function() {
-		$('ol#items').append("<li>"+$('#item').val()+"</li>").append("<span class='label pending'>Pending</span>")
+		$('ol#items').append("<span class='label pending'>Pending</span>").append("<li>"+$('#item').val()+"</li>")
 		$('#item').val("") 
 		$('#item').focus()
 		});
+
+	$(document).on('keypress', function(e) {
+    		if(e.which == 13) {
+				$('ol#items').append("<span class='label pending'>Pending</span>").append("<li>"+$('#item').val()+"</li>")
+				$('#item').val("") 
+				$('#item').focus()
+				}
+			});
 	}
 
-//Question: why am I getting nested labels and how do I remove the pending one?
+
 function completeItem(item) {
 	$(document).on('click', '.label', function() {
-		$(".label.pending").attr(".label.success")
-		$(this).append("<span class='label success'>Done!</span>")
+		$(this).addClass("success").removeClass("pending")
+		$(this).text("Done!")
+		updateTotal()
 		});
 	}
+
+
+function updateTotal() {
+	if ($('.listTotal').length == 0 ) {
+		$('ol#items').append("<div class='listTotal'></div>")
+	}
+	$('.listTotal').text("Completed : " + $('.success').length + "   Pending : " + $('.pending').length)
+}
+
+
+
+//I want to create a div
+//I want to put text + count inside
+//I want to append that underneath my list
+
+
 
 
 //JQuery method - wrapped to wait for page load
 $(document).ready(addToList())
 $(document).ready(completeItem())
+$(document).ready(updateTotal())
