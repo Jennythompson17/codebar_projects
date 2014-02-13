04886ac8ff0d0c12930c5764f82af3d4c9a7648f
JennyThompson17 <jennythompson17@gmail.com>
2014-02-13 18:16:51 +0000

    input color and change preview background

--- Colour picker/script.js
+++ Colour picker/script.js
@@ -3,13 +3,15 @@
 
 // set background color
 function setPreviewColor(color) {
-	$(document).on('click change','.preview', function() {
-		$(this).css('background-color','green' )
+	$(document).on('click','#add-to-favorite', function() {
+		var color = $('#inner-editor').val()
+		$('.preview').css('background-color',$('#color').val() )
 	}); 
 }
 
-//Input text and keypress or keydown or keyup
-//Grab the preview div
-//Change the color class
+//Input text
+// on keypress or keydown or keyup
+	//Grab the preview div
+	//Change the color class
 
 $(document).ready(setPreviewColor())
