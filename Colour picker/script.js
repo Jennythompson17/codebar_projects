bb4f44db6431b7eaf3eec88ac385e2d4b912c0b5
JennyThompson17 <jennythompson17@gmail.com>
2014-02-26 20:25:00 +0000

    finish color picker!

--- Colour picker/script.js
+++ Colour picker/script.js
@@ -2,14 +2,50 @@
 
 
 // set background color
-function setPreviewColor(color) {
-	$(document).on('keydown keypress keyup','#color', function() {
-		$('.preview').css('background-color',$('#color').val() )
+function setPreviewColor(previewColor) {
+	$('.preview').css('background-color',previewColor );
+	$('.color-code').text($('.preview').css('background-color'));
+}
 
-	}); 
-
+// add a box to the beginning of #colors
+function addBox(colour) {
+ 	$('#colors').prepend("<div class='item' style='background-color: " + colour + ";'><div>");
+ 	$('#color').focus();
 }
 
 
 
-$(document).ready(setPreviewColor())
+$(document).ready(function(){
+	var colors = ['red','blue','green','orange','hotpink', 'silver','brown','black','purple','yellow','teal','grey'];
+	var random_position = Math.floor( Math.random() * colors.length );
+	var random_color = colors[random_position];
+
+	$.each(colors, function(index, colour) {
+		addBox(colour);
+	});
+
+	setPreviewColor(random_color);  
+	$(document).on('change','#color', function() {
+		var favouriteColor = $('#color').val();
+		setPreviewColor(favouriteColor);
+	});
+	
+	$(document).on('click','#add-to-favorite', function(){
+		var favouriteColor = $('.preview').css('background-color');
+		var length = $('#colors .item').length;
+		if (length == 16) {
+			$('#colors .item').last().remove();
+		}
+		addBox(favouriteColor);
+	});
+	var currentColor;
+	$(document).on('mouseenter','#colors .item',  function(){
+		currentColor = $('.preview').css('background-color');
+		var hoverColor = $(this).css('background-color');
+		setPreviewColor(hoverColor);
+
+	}).on('mouseleave', '#colors .item', function() {
+		setPreviewColor(currentColor);
+	});
+
+});
