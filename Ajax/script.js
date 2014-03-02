b27fd7a950f02bd40f35997b4c488917d49b539c
JennyThompson17 <jennythompson17@gmail.com>
2014-03-02 08:34:32 +0000

    stored username as variable

--- Ajax/script.js
+++ Ajax/script.js
@@ -1,12 +1,14 @@
 // github user finder example
 
-var url = 'https://api.github.com/users/jennythompson17';
+var userName = 'jennythompson17'; 
+var url = 'https://api.github.com/users/'+ userName;
 
 function getData() {
  var xmlhttp = new XMLHttpRequest();
  xmlhttp.onload = function(){
  	var userData = JSON.parse(this.responseText);
  	console.log(userData);
+ 	console.log(userData.login);
  };
  xmlhttp.open("GET", url, true);
  xmlhttp.send();
