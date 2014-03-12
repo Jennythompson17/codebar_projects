056a9020166caa83b1705b621f33eacd1229897b
JennyThompson17 <jennythompson17@gmail.com>
2014-03-12 20:10:08 +0000

    event handling via JQuery

--- Ajax/script.js
+++ Ajax/script.js
@@ -1,24 +1,39 @@
 // github user finder example
 
+function updatePage(userData, response, person){
+	if(response!=404){
+		console.log(userData);
+ 		console.log(userData.login);
+ 		}
+	else{
+		$("#profile h2").html("no such user: " + person);
+		};
+	};
 
-function getData() {
+
+function getData(person) {
  var xmlhttp = new XMLHttpRequest();
  xmlhttp.onload = function(){
  	var userData = JSON.parse(this.responseText);
- 	console.log(userData);
- 	console.log(userData.login);
+ 	var response = JSON.parse(this.status);
+ 	updatePage(userData, response, person);
  };
+
+ var url = 'https://api.github.com/users/'+ person;
  xmlhttp.open("GET", url, true);
  xmlhttp.send();
  return xmlhttp;
 };
 
+
 $(document).ready(function(){
   $(document).on('keypress', '#username', function(e){
     if (e.which == 13) {
-      person = $('#username').val();
-      url = 'https://api.github.com/users/'+ person;
-      getData();
+      var person = $('#username').val();
+      getData(person);
     }
   })
 });
+
+
+//$("#profile h2").html("no such user: " + person)
