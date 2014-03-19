13d9b463b16083860e6694a33f0e5ee0b24a16f3
JennyThompson17 <jennythompson17@gmail.com>
2014-03-19 21:00:29 +0000

    it's working!

--- Thom's Widget/script.js
+++ Thom's Widget/script.js
@@ -1,14 +1,5 @@
 /* Widget for Thom */
 
-
-//array of description options
-//$(document).ready(function(){
-	//var adjectives = ['innovative','disruptive','visionary'];
-	//var random_position = Math.floor( Math.random() * adjectives.length );
-	//var random_adjective = adjectives[random_position];
-
-	//$.each(adjectives, function(index, adjective) {
-		//addBox(adjective);
 var adjectives = ['innovative','disruptive','visionary'];
 var firstPhrases = ['running towards a goal most call impossible', 'toiling in the service of a common good', 'fusing talent with technology'];
 		
@@ -23,19 +14,12 @@ function getImpact(item) {
 	var random_adjective = getStory(adjectives);
 	var random_first = getStory(firstPhrases);
 
-	$(document).on('click','#add-to-list' , function(e) {
-				$('#items').html($('#item').val()+ ' the ' + random_adjective + ' though-leader is passionate about ' + random_first)
-				$('#item').val("") 
-				$('#item').focus()
+	$(document).bind('submit','#form' , function(e) {
+			$('#items').html($('#item').val()+ ' the ' + random_adjective + ' though-leader is passionate about ' + random_first)
+			$('#item').val("") 
+			$('#item').focus()
+			e.preventDefault();
 		});
-
-	$(document).on('keypress', function(e) {
-    		if(e.which == 13) {
-				$('#items').html($('#item').val()+ ' the ' + random_adjective + ' though-leader is passionate about ' + random_first)
-				$('#item').val("") 
-				$('#item').focus()
-				}
-			});
 	}
 
 
@@ -51,3 +35,5 @@ $(document).ready(getImpact())
 //3) pull name from input box
 //4) concatenate name + "is" + [random adjective from array]
 //5) use .html() to display this on the screen
+
+//Gender - use "they" and replace with "him" or "her"
