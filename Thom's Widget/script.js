22f384712f3b2d952f22d8c2450203ba7bf6945d
JennyThompson17 <jennythompson17@gmail.com>
2014-03-22 09:18:45 +0000

    added to arrays and statements, added spacer divs

--- Thom's Widget/script.js
+++ Thom's Widget/script.js
@@ -2,14 +2,22 @@
 
 //Questions
 //1) how do I attach a re-load event handler to thesubmit button for submissions after the first one?
+//2) How do I mak sure that random doesn't choose the same adverb?
+//3) how do I handle a re-submit where the input boxes are empty?
 
 
 var adjectives = ['lean', 'agile', 'innovative', 'disruptive', 'impactful', 'cutting-edge', 'sustainable', 'avid', 'adaptive', 'nimble', 'practical', 'accessible', 'people-centred', 'talented', 'supportive', 'authentic', 'fearless', 'excited', 'diverse', 'open', 'digital', 'experimental', 'dedicated', 'progressive', 'visionary', 'world-class', 'iconic', 'resilient', 'inspirational', 'compelling', 'hard-hitting', 'fruitful', 'credible', 'evidenced', 'immense', 'creative', 'awe-inspiring', 'sudden'];
 var descriptor = [ 'adventurer', 'individual', 'communicator', 'public speaker', 'event planning genius', 'genius', 'designer', 'social designer', 'facilitator', 'logistics and numbers ninja', 'expert', 'social expert', 'guru', 'venture hunter', 'contrarian communicator', 'instigator', 'grassroots volunteer', 'world roamer', 'pop-up restaurateur', 'campaigner', 'connector', 'changemaker', 'digital change agent', 'early interventionist', 'social intrapreneur', 'lover of life']
-var firstAim = ['running towards a goal most call impossible', 'toiling in the service of a common good', 'fusing talent with technology', 'making a difference in the world', 'creating new movements', 'institutions and companies', 'inspiring organisations across business and society'];
-var cause =[ 'our global mission to improve', 'our working together for', 'the building of partnerships with', 'the turning of inventions into marketable applications for', 'the improvement of the innovative capacity of', 'the development of practical insights in the service of', 'the deep connection with amazing people who are impassioned about', 'the potential to harness the talents and skills of diverse people for' ]
-var who =['people', 'public services', 'the user experience', 'everyone\'s health', 'commercial cultural and social potential','equal access to education for all', 'improving outcomes', 'a hub for innovators the world over', 'exciting people-driven startups', 'making a difference in the world', 'the future', 'children', 'young people', 'information', 'the information age', 'growing a stronger society', 'solving society\'s intractable problems', 'the obesity epidemic', 'women', 'men', 'young men', 'young women', 'better answers to our biggest challenges', 'improved fitness', 'old people', 'a more equal world', 'connecting visionary companies', 'experiential learning', 'leadership', 'social innovation', 'digital disruption', 'leadership resilience', 'connectivity', 'change', 'a global coalition for peace', 'a more free world', 'the innovations of the future', 'humanity', 'us all', 'wellbeing', '21st century learning', 'radical efficiency'
-]
+var firstAim = ['running towards a goal most call impossible', 'toiling in the service of a common good', 'fusing talent with technology', 'making a difference in the world', 'creating new movements', 'crafting institutions and companies', 'inspiring organisations across business and society'];
+var cause =[ 'our global mission to improve', 'our working together for', 'the building of partnerships with', 'the turning of inventions into marketable applications for', 'the improvement of the innovative capacity of' ]
+var who =['people', 'public services', 'the user experience', 'everyone\'s health', 'commercial cultural and social potential','equal access to education for all', 'improving outcomes', 'a hub for innovators the world over']
+var cause2 =['the development of practical insights in the service of', 'the deep connection with amazing people who are impassioned about', 'the potential to harness the talents and skills of diverse people for' ]
+var adverb =['creatively', 'impactfully', 'passionately', 'nimbly', 'supportively', 'swiftly', 'accessibly', 'progressively', 'excitedly', 'inspirationally', 'digitally', 'compellingly', 'actively', 'keenly']
+var verb =[ 'innovates', 'creates', 'co-creates', 'co-produces', 'disrupts', 'supports', 'improves', 'sustains', 'grows', 'scales', 'replicates', 'designs', 'fosters']
+var noun = ['talent', 'skill', 'power', 'inventiveness', 'commitment', 'passion', 'love', 'flair']
+var verb2 = ['incubating', 'helping', 'thriving', 'actualising','nurturing', 'changing', 'accelerating', 'improving']
+var secondAim = ['exciting people-driven startups', 'making a difference in the world', 'the future', 'children', 'young people', 'information', 'the information age', 'growing a stronger society', 'solving society\'s intractable problems', 'the obesity epidemic', 'women', 'men', 'young men', 'young women', 'better answers to our biggest challenges', 'improved fitness', 'old people', 'a more equal world', 'connecting visionary companies', 'experiential learning', 'leadership', 'social innovation', 'digital disruption', 'leadership resilience', 'connectivity', 'change', 'a global coalition for peace', 'a more free world', 'the innovations of the future', 'humanity', 'us all', 'wellbeing', '21st century learning', 'radical efficiency']
+
 //why doesn't this work? - check document ready at bottom
 function questions(){
 	$('#item').val("what is your name?");
@@ -29,17 +37,26 @@ function getImpact(item) {
 	var random_first = getStory(firstAim);
 	var random_cause = getStory(cause);
 	var random_who =getStory(who);
-	var random_cause2 = getStory(cause);
+	var random_cause2 = getStory(cause2);
 	var random_who2 =getStory(who);
+	var random_adverb = getStory(adverb);
+	var random_verb = getStory(verb);
+	var random_cause3 = getStory(cause);
+	var random_who3 =getStory(who);
+	var random_noun = getStory(noun);
+	var random_verb2 = getStory(verb2);
+	var random_second = getStory(secondAim);
 
-	var name
 
 	//this is the leading sentence
 	$(document).bind('submit','#form' , function(e) {
 		var name = $('#item').val();
 		var years = $('#years').val();
 			$('#items').html(name + ', the ' + random_adjective + ' ' + random_descriptor+ ', has spent the last ' + years+ ' years '+ random_first+'. ')
-			$('#items2').html(name + ' is passionate about ' + random_cause + ' '+ random_who+ ' and'+ random_cause2+ ' '+ random_who2+ '.')
+			$('#items2').html(name + ' is passionate about ' + random_cause + ' '+ random_who+ ' and '+ random_cause2+ ' '+ random_who2+ '. '+ name + ' '+ random_adverb + ' '+ random_verb + ' '+random_cause3+ ' '+ random_who3 + '.')
+			$('#items3').html(name + '\'s '+ random_noun+ ' for '+ random_verb2 + ' '+ random_second )			
+
+
 			$('#item').val("") 
 			$('#item').focus()
 			$('#years').val("") 
