5084f418201c9b9051ccfac624733bf0d73d58c0
JennyThompson17 <jennythompson17@gmail.com>
2014-06-07 07:35:27 +0100

    Add Ajax 3 tutorial

--- Ajax 2/script.js
+++ Ajax 2/script.js
@@ -1,54 +1,66 @@
 // TV Schedule example
+    $(document).on('click', '#header', function() {
+      // AJAX call using JQuery that retrieve and process the result
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onload = function(){
+       var response = JSON.parse(this.responseText);
+       var categories = response['categories'];
+       //var title_first_object = response['categories'][0].title;
+      //console.log('response is', title_first_object);
+      listGenres(categories);
+     };
 
-function retrieveGenres() {
-  // AJAX call using JQuery that retrieve and process the result
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onload = function(){
-   var response = JSON.parse(this.responseText);
-   var categories = response['categories'];
-   var title_first_object = response['categories'][0].title;
-  //console.log('response is', title_first_object);
-  listGenres(categories);
- };
+     var query = 'http://www.bbc.co.uk/tv/programmes/genres.json';
+      xmlhttp.open("GET", query, true);
+      xmlhttp.send();
+     return xmlhttp;
+     //console.log(xmlhttp);
 
- var query = 'http://www.bbc.co.uk/tv/programmes/genres.json';
-  xmlhttp.open("GET", query, true);
-  xmlhttp.send();
- return xmlhttp;
- //console.log(xmlhttp);
+    function listGenres(categories){
+      $.each(categories, function(index, category){
+        $('#genres').append(
+            $("<li>").text(category.title)
+                     .data("genre", category.key)
+                    )
+        console.log( index + ":" + category.title);
 
-}
+      });
+  };
+});
 
-function listGenres(categories){
-  $.each(categories, function(index, category){
-    $('#genres').append("<ul>"+category.title+"</ul>");
-    console.log( index + ":" + category.title);
+$(document).on('click', 'li', function() {
+   var genre = $(this).data("genre");
+   console.log(genre);
+   getTomorrowsSchedule(genre);
 
-  });
-}
+  function getTomorrowsSchedule(genre) {
+   // call to retrieve TV schedule
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onload = function(){
+     var scheduleResponse = JSON.parse(this.responseText);
+     var broadcasts = scheduleResponse['broadcasts'];
+     //var title_first_object = scheduleResponse['broadcasts'][0]['programme']['title'];
+    //console.log('response is', title_first_object);
+    listProgrammes(broadcasts);
+   };
 
-function getTomorrowsSchedule(genre) {
- // call to retrieve TV schedule
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onload = function(){
-   var scheduleResponse = JSON.parse(this.responseText);
-   var broadcasts = scheduleResponse['broadcasts'];
-   //var title_first_object = scheduleResponse['broadcasts'][0]['programme']['title'];
-  //console.log('response is', title_first_object);
-  listProgrammes(broadcasts);
- };
-
- var query2 = 'http://www.bbc.co.uk/tv/programmes/genres/comedy/schedules/tomorrow.json';
-  xmlhttp.open("GET", query2, true);
-  xmlhttp.send();
- //return xmlhttp;
-}
+   var query2 = 'http://www.bbc.co.uk/tv/programmes/genres/'+ genre +'/schedules/tomorrow.json';
+    xmlhttp.open("GET", query2, true);
+    xmlhttp.send();
+   //return xmlhttp;
+  }
+ });
 
 function listProgrammes(broadcasts){
   $.each(broadcasts, function(index, broadcast){
     var durationAdjusted = broadcast.duration/60;
     $('#programmes').append("<li>"+broadcast.programme.display_titles.title+"</li>");
-    $('#programmes').append("<img src=http://ichef.bbci.co.uk/images/ic/272x153/"+broadcast.programme.image.pid+".jpg />");
+    if (broadcast.programme.image){
+        $('#programmes').append("<img src=http://ichef.bbci.co.uk/images/ic/272x153/"+broadcast.programme.image.pid+".jpg />");
+    }
+    else{
+        $('#programmes').append("<img src='http://placehold.it/272x153' />");
+    }
     $('#programmes').append("<h2> Synopsis: "+broadcast.programme.short_synopsis+"</h2>");
     $('#programmes').append("<h2> Date: "+formatDate(broadcast.start, broadcast.end)+"</h2>");
     $('#programmes').append("<h2> Programme Duration: "+durationAdjusted+" minutes</h2>");
---
+++ Ajax 3/script.js
@@ -0,0 +1,43 @@
+/** Hangman **/
+
+//
+function update (data){
+	$('.token').val(data.token);
+	$('.hangman-word').text(data.hangman);
+};
+
+
+
+$(document).ready( function(){
+	$('#new-game').click( function() {
+		$.post("http://hangman-api.herokuapp.com/hangman").success(function( data ){ 
+			$('#new-game').hide();
+			update (data);
+		});
+
+	});
+
+	$('#guess').click( function() {
+		var letter =$('.letter').val();
+		$.ajax({
+			type: 'put',
+			url: 'http://hangman-api.herokuapp.com/hangman',
+			data: { token: $('.token').val(), letter: letter },
+		}).success(function( data ){ 
+			console.log(data);
+			update(data);
+			if (!data.correct) {
+				$('.attempts').append($('<span class="wrong"/>').text(letter));
+				}
+			else{
+				$('.attempts').append($('<span/>').text(letter));
+			}
+			var wrongGuesses = $('.wrong').length+1;
+
+			$('.remaining').text(7-wrongGuesses);
+
+		});
+
+	});
+
+});
