a29bff9951d5db6402432bb53c2af9ece8ce9dd5
JennyThompson17 <jennythompson17@gmail.com>
2014-06-07 11:44:54 +0100

    updates

--- Ajax 3/script.js
+++ Ajax 3/script.js
@@ -8,9 +8,14 @@ function update (data){
 
 
 $(document).ready( function(){
+	$('.console').hide();
+	$('.remaining-guesses').hide();
 	$('#new-game').click( function() {
 		$.post("http://hangman-api.herokuapp.com/hangman").success(function( data ){ 
 			$('#new-game').hide();
+			$('.intro').hide();
+			$('.console').show();
+			$('.remaining-guesses').show();
 			update (data);
 		});
 
@@ -36,17 +41,20 @@ $(document).ready( function(){
 			$('.remaining').text(7-wrongGuesses);
 
 			if ($('.remaining').html() == 0 ) {
+
 				$('.console').hide();
 				update(data);
+
 				$.ajax({
 					type: 'get',
 					url: 'http://hangman-api.herokuapp.com/hangman',
 					data: { token: $('.token').val()}
 				}).success(function( data ){ 
 					update (data);
-					console.log(data.solution);
+
 					$('.hangman-word').text(data.solution);
 					$('#new-game').show();
+
 					$('#new-game').click( function() {
 						$('.console').show();
 						$('.attempts').empty();
