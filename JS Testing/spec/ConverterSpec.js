816cc2c51d690e110b87b728aea34647eafd1739
U-CORP\jennifer.thompson <jennifer.thompson@LON04LT66.corp.local>
2014-10-22 20:43:48 +0100

    Add JS Testing

---
+++ JS Testing/spec/ConverterSpec.js
@@ -0,0 +1,11 @@
+describe("Cooking Ingredients Converter", function() {
+
+  describe("Temperatures", function() {
+    it("Fahrenheit to Celsius", function() {
+      var result = convertFahrenheitToCelsius(100);
+
+      expect(result).toEqual('37.8');
+
+    });
+  });
+});
---
+++ JS Testing/src/Converter.js
@@ -0,0 +1,8 @@
+/** Unit Converter **/
+function convertFahrenheitToCelsius(temperature) {
+  //conversion code
+  var farenheit = temperature;
+  var celsius = (farenheit - 32)/1.8
+  return celsius.toFixed(1);
+  
+}
