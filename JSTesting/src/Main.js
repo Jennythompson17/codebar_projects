6a0804fe027201ec9a7a80650417fc650ade9376
Jenny <Jennythompson17@gmail.com>
2014-10-29 21:12:46 +0000

    add bootstrap styling

--- JSTesting/src/Main.js
+++ JSTesting/src/Main.js
@@ -1,40 +1,41 @@
 /** Calculator Functions **/
 		
 window.onload = function(){
-	var button = document.getElementById("button");
 	var input = document.getElementById("input");
 	var output = document.getElementById("output");
 	var choice = document.getElementById("convertingWhat");
 
-		button.onclick=function(){
+		document.getElementById("button").onclick=function(){
 			switch(choice.value) {
 				case "Fahrenheit to Celsius":
-					var temperature = input.value;
-					output.value=convertFahrenheitToCelsius(temperature);
+					output.value=convertFahrenheitToCelsius(input.value);
 					break;
 
 				case "Celsius to Fahrenheit":
-					var temperature = input.value;
-					output.value=convertCelsiustoFahrenheit(temperature);
+					output.value=convertCelsiustoFahrenheit(input.value);
 					break;
 
 				case "Pounds to Kilos":
-					var pounds = input.value;
-					output.value=convertPoundstoKilos(pounds);
+					output.value=convertPoundstoKilos(input.value);
 					break;
 
 				case "Litres to Gallons":
-					var litres = input.value;
-					output.value=convertLitrestoGallons(litres);
+					output.value=convertLitrestoGallons(input.value);
 					break;
 
 				case "Miles to Km":
-					var miles = input.value;
-					output.value=convertMilesToKm(miles);
+					output.value=convertMilesToKm(input.value);
 					break;
 				
 			};
         
       	};
+
+      	choice.onchange=function(){
+      		input.value = "";
+      		output.value = "";
+      	};
+
+
 
     };
