e41f211f1a1c2c4c96363486e6c584233db19f13
Jenny <Jennythompson17@gmail.com>
2014-10-29 20:19:04 +0000

    add main.js

---
+++ JSTesting/src/Main.js
@@ -0,0 +1,16 @@
+/** Calculator Functions **/
+
+var button = document.getElementById("button");
+var input = document.getElementById("input");
+var output = document.getElementById("output");
+var choice = document.getElementById("convertingWhat");
+
+button.onclick=function(){
+	switch(choice) {
+		case 0:
+			choice = "Fahrenheit to Celsius";
+
+			break;
+		};
+        
+      };
