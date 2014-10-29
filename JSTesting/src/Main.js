1e83a511a707306070f4eba05171854d1399b9a5
Jenny <Jennythompson17@gmail.com>
2014-10-29 20:53:49 +0000

    add case statement

--- JSTesting/src/Main.js
+++ JSTesting/src/Main.js
@@ -1,16 +1,40 @@
 /** Calculator Functions **/
+		
+window.onload = function(){
+	var button = document.getElementById("button");
+	var input = document.getElementById("input");
+	var output = document.getElementById("output");
+	var choice = document.getElementById("convertingWhat");
 
-var button = document.getElementById("button");
-var input = document.getElementById("input");
-var output = document.getElementById("output");
-var choice = document.getElementById("convertingWhat");
+		button.onclick=function(){
+			switch(choice.value) {
+				case "Fahrenheit to Celsius":
+					var temperature = input.value;
+					output.value=convertFahrenheitToCelsius(temperature);
+					break;
 
-button.onclick=function(){
-	switch(choice) {
-		case 0:
-			choice = "Fahrenheit to Celsius";
+				case "Celsius to Fahrenheit":
+					var temperature = input.value;
+					output.value=convertCelsiustoFahrenheit(temperature);
+					break;
 
-			break;
-		};
+				case "Pounds to Kilos":
+					var pounds = input.value;
+					output.value=convertPoundstoKilos(pounds);
+					break;
+
+				case "Litres to Gallons":
+					var litres = input.value;
+					output.value=convertLitrestoGallons(litres);
+					break;
+
+				case "Miles to Km":
+					var miles = input.value;
+					output.value=convertMilesToKm(miles);
+					break;
+				
+			};
         
-      };
+      	};
+
+    };
