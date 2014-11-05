624a87a91ae49bc2367df098c1a5036d5ce02e0d
Jenny <Jennythompson17@gmail.com>
2014-11-05 21:11:12 +0000

    add function

--- coffeeApp/js/main.js
+++ coffeeApp/js/main.js
@@ -2,6 +2,30 @@ var app;
 
 app = angular.module('CoffeeShopFinder', true);
 
-app.conroller('FinderCtrl', function($scope, $http, $location, $windwow){
+app.controller('FinderCtrl', function($scope, $http, $location, $window){
+	$scope.findShops = function() {
+		foundGeocode = function(response) {
+			lat = response.data.results[0]['geometry']['location']['lat']
+			lng = response.data.results[0]['geometry']['location']['lng']
 
-};
+			$http({
+				method:"get",
+				url:" https://api.foursquare.com/v2/venues/search?oauth_token=IMLGBRBQ0CRH4XICMZPURTJ2OT3DOH5QJYS3XKM3QR32LYET&v=20141105&ll="+lat + ","+lng
+			}).then(foundShops, (function(){}));
+		};
+
+		foundShops = function(response){
+			debugger
+		};
+
+		$http({
+			method:"get",
+			url:"https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDpq8m_cmrhDzVAMLxZFvdxGK-tbHjvXIA&address="+$scope.searchTerm
+		}).then(foundGeocode, (function(){}));
+
+
+
+		console.log($scope.searchTerm)
+	};
+
+});
