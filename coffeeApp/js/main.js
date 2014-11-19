6b2f2731a1340edef7dd746d18a28a7f72727768
JennyThompson17 <jennythompson17@gmail.com>
2014-11-19 21:01:28 +0000

    printing coffee shop names

--- coffeeApp/js/main.js
+++ coffeeApp/js/main.js
@@ -2,33 +2,44 @@
 
 var app;
 
-app = angular.module('CoffeeShopFinder', true);
+app = angular.module('CoffeeShopFinder', []);
 
-		foundGeocode = function(response) {
-app.controller('FinderCtrl', function($scope, $http, $location, $window){
-	$scope.findShops = function() {
-			lat = response.data.results[0]['geometry']['location']['lat']
-			lng = response.data.results[0]['geometry']['location']['lng']
+		app.controller('FinderCtrl', function($scope, $http, $location, $window){
+			//var foundGeocode = function(response) {
+			$scope.findShops = function() {
+				var getLatLong = function(address){
+	 				$http({
+	 					method:"get",
+	 					url:"https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDpq8m_cmrhDzVAMLxZFvdxGK-tbHjvXIA&address="+address
+	 					}).then(function(response){
+	 						lat = response.data.results[0]['geometry']['location']['lat']
+							lng = response.data.results[0]['geometry']['location']['lng']
+// check variables
+						console.log(lat + "," + lng);
+						locateCoffee (lat, lng);
+	 					});
+	 			};
 
-			$http({
-				method:"get",
-				url:" https://api.foursquare.com/v2/venues/search?oauth_token=IMLGBRBQ0CRH4XICMZPURTJ2OT3DOH5QJYS3XKM3QR32LYET&v=20141105&ll="+lat + ","+lng
-			}).then(foundShops, (function(){}));
-		};
+				var locateCoffee = function(lat, lng){
+					$http({
+					 	method:"get",
+					 	url:" https://api.foursquare.com/v2/venues/search?oauth_token=IMLGBRBQ0CRH4XICMZPURTJ2OT3DOH5QJYS3XKM3QR32LYET&v=20141105&categoryId=4bf58dd8d48988d1e0931735&ll="+lat + ","+lng
+					 	}).then(function(response){
+					 		var allResultsJSON = response.data['response']['venues'];
+					 		allResultsJSON.forEach(function(venue){
 
-		foundShops = function(response){
-			debugger
-		};
+					 		console.log(venue['name'])
+					 		});
+					 	});
 
-		$http({
-			method:"get",
-			url:"https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDpq8m_cmrhDzVAMLxZFvdxGK-tbHjvXIA&address="+$scope.searchTerm
-		}).then(foundGeocode, (function(){}));
+				};
+						getLatLong ($scope.searchTerm);
+			};
+	
 
 
-
-		console.log($scope.searchTerm)
-	};
+// Log search term
+					//console.log($scope.searchTerm);
 
 });
 
